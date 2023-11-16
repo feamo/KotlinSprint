a02cee0c28d962e6a8e2f6c3a20ec16aae50706f
Kolerova Diana <kolerova@sbermegamarket.ru>
2023-11-16 22:12:23 +0300

    lesson1 task2 add employee count and logs

---
+++ src/main/kotlin/lesson1/lesson1-2.kt
@@ -0,0 +1,18 @@
+package lesson1
+
+fun main(){
+
+    val ordersCount: Int = 75
+    val notifyText: String = "Thank you"
+
+    println(ordersCount)
+    println(notifyText)
+
+    var employeeCount: Int = 200
+//    println(employeeCount)
+
+    employeeCount = 199
+    println(employeeCount)
+
+
+}
