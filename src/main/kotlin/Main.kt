be633636506057c7675f217b2bbc6445d05b9920
Kolerova Diana <kolerova@sbermegamarket.ru>
2023-11-16 22:48:34 +0300

    lesson1 task3 create variables and autputting data about the first space flyght

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
 fun main() {
-    
+    val yearOfFlight: Int = 1961
+    var hour = "09"
+    var minute = "07"
+
+    println("""
+        |Полет Гагарина состоялся в $yearOfFlight году.
+        |Время взлета $hour:$minute 
+    """.trimMargin()
+    )
+
+    hour = "10"
+    minute = "55"
+
+    println("Время посадки $hour:$minute")
+
 }
