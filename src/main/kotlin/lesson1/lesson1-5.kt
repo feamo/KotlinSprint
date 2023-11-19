0e4838a74d1b8235851ac05c4760a8927f9de9ff
Kolerova Diana <kolerova@sbermegamarket.ru>
2023-11-19 15:47:03 +0300

    lesson1-task5 Time spent in space

--- src/main/kotlin/lesson1/lesson1-5.kt
+++ src/main/kotlin/lesson1/lesson1-5.kt
@@ -1,20 +1,14 @@
 package lesson1
 
 fun main(){
-    val secondsCount: Int = 6480
-    val secondsOnHour: Int = 3600
-    var hoursInFlyght: Int = secondsCount / secondsOnHour
-//    println(hoursInFlyght)
+    val totalSeconds = 5678
 
-    val secondsInMinutesOfFlyght: Int = secondsCount % secondsOnHour
-//    println(secondsInMinutesOfFlyght)
+    val hours = totalSeconds / 3600
+    val minutes = (totalSeconds % 3600) / 60
+    val seconds = totalSeconds % 60
 
-    val minuteInFlyght: Int = secondsInMinutesOfFlyght / 60
-//    println(minuteInFlyght)
+    val timeInSpace = "%02d:%02d:%02d".format(hours, minutes, seconds)
 
-    val secondsInFlyght: Int = secondsCount % minuteInFlyght
-    println(secondsInFlyght)
-
-    println("Время в полете: $hoursInFlyght:$minuteInFlyght:$secondsInFlyght")
+    println("Время, проведенное в космосе: $timeInSpace")
 
 }
