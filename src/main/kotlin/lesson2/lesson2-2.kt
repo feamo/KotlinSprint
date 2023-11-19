437fedea385812431523870b023569f50608e793
Kolerova Diana <kolerova@sbermegamarket.ru>
2023-11-19 17:11:43 +0300

    lesson2-task2 payroll calculation

--- src/main/kotlin/lesson2/lesson2-2.kt
+++ src/main/kotlin/lesson2/lesson2-2.kt
@@ -1,5 +1,16 @@
 package lesson2
 
 fun main(){
+//    averageScore
+    val totalEmployee = 50
+    val totalTrainee = 30
 
+    val salaryEmployee = 30000
+    val salaryTrainee = 20000
+
+    val totalCountSalaryEmployee = totalEmployee * salaryEmployee
+    val totalCountSalary = (totalEmployee * salaryEmployee) + (totalTrainee * salaryTrainee)
+    val averageSalary = totalCountSalary / (totalEmployee+totalTrainee)
+
+    println( "totalCountSalaryEmployee: $totalCountSalaryEmployee, totalCountSalary: $totalCountSalary, averageSalary: $averageSalary")
 }
