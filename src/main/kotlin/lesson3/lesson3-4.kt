62b42a21a3d82364581874ddeedad4a73c280cd9
Kolerova Diana <kolerova@sbermegamarket.ru>
2023-11-22 01:54:07 +0300

    lesson3-4 shees

---
+++ src/main/kotlin/lesson3/lesson3-4.kt
@@ -0,0 +1,15 @@
+package lesson3
+
+fun main() {
+
+    var from = "E2"
+    var to = "E4"
+    var moves = 1
+
+    println("[$from - $to, $moves]")
+
+    from = "D2"
+    moves = 2
+    println("[$from - D${2 + 1}, $moves]")
+
+}
