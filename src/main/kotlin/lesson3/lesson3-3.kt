badcc0b51efa29463769aac8a5e8c773b33d3e66
Kolerova Diana <kolerova@sbermegamarket.ru>
2023-11-22 01:42:59 +0300

    lesson3-3 multiplication table

---
+++ src/main/kotlin/lesson3/lesson3-3.kt
@@ -0,0 +1,18 @@
+package lesson3
+
+fun main() {
+    val number = 3
+
+    println("""
+         $number x 1 = ${number*1}
+         $number x 2 = ${number*2}
+         $number x 3 = ${number*3}
+         $number x 4 = ${number*4}
+         $number x 5 = ${number*5}
+         $number x 6 = ${number*6}
+         $number x 7 = ${number*7}
+         $number x 8 = ${number*8}
+         $number x 9 = ${number*9}
+         $number x 10 = ${number*10}
+    """.trimIndent())
+}
